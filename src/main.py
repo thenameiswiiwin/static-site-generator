ac8246efb673542596c5ecff511221b1cacfd041
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-05-04 01:54:36 -0400

    feat: add TextNode unit tests

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from textnode import TextNode, TextType
 
+
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
+
 
 main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,23 +1,28 @@
 from enum import Enum
 
+
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
 
+
 class TextNode:
-    def __init__(self, text: str, text_type: TextType, url: str = None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
-        if not isinstance(other, TextNode):
-            return False
-        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
 
     def __repr__(self):
-        return f"TextNode({self.text, self.text_type, self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
